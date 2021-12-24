c2866ece8b5b2db576ac9dc167ecf34206eef220
Rafa Ruiz <raruizmu@gmail.com>
2021-12-24 18:31:53 +0100

    Bring back the semitransparentModal

--- lib/ui/screens/set_up/set_up_screen.dart
+++ lib/ui/screens/set_up/set_up_screen.dart
@@ -31,8 +31,7 @@ class SetUpScreen extends StatelessWidget {
         vertical: large7,
         horizontal: large1,
       ),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(mainModalRadius)),
+      child: semiTransparentModal(
         child: Container(
           padding: const EdgeInsets.symmetric(vertical: large2, horizontal: large1),
           child: Column(
