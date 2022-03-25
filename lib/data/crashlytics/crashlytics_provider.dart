61aa52cb15e83da647d51b09b0e1d49c580832e1
Rafa <raruizmu@gmail.com>
2022-03-25 16:02:59 +0100

    Do not log in debug mode

--- lib/data/crashlytics/crashlytics_provider.dart
+++ lib/data/crashlytics/crashlytics_provider.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CrashlyticsProvider {
   CrashlyticsProvider() {
     Firebase.initializeApp().then(
       (_) {
-        FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+        if (!kDebugMode) {
+          FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+        }
       },
     );
   }
